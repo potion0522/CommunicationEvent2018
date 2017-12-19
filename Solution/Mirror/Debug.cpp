5e7073ef61ec5726395c2d9c63676665bed63a5d
tika <a>
2017-12-19 21:14:41 +0900

    Debugにログを出せるようにした

--- Solution/Mirror/Debug.cpp
+++ Solution/Mirror/Debug.cpp
@@ -1,21 +1,23 @@
 #include "Debug.h"
 #include "DxLib.h"
-#include "GlobalData.h"
+#include "const.h"
 #include <time.h>
 
 const int INIT_X = 20;
 const int INIT_Y = 0;
 
 Debug::Debug( ) {
+	initialize( );
 }
 
 Debug::~Debug( ) {
 }
 
 void Debug::initialize( ) {
+	_color = std::shared_ptr< Color >( new Color( ) );
 }
 
-void Debug::error( char *err ) {
+void Debug::error( std::string err ) {
 	if ( fopen_s( &_fp, "error.txt", "w" ) != 0 ) {
 		printfDx( "Error：ファイルオープンに失敗しました。" );
 		exit( 0 );
@@ -28,7 +30,7 @@ void Debug::error( char *err ) {
 	int len = ( int )strlen( buf );
 	buf[ len - 1 ] = '\0';
 
-	fprintf_s( _fp, "[ %s ] %s", buf, err );
+	fprintf_s( _fp, "[ %s ] %s", buf, err.c_str( ) );
 	fclose( _fp );
 
 	DxLib_End( );
@@ -36,4 +38,23 @@ void Debug::error( char *err ) {
 }
 
 void Debug::update( ) {
+	printLog( );
+	initLog( );
+}
+
+void Debug::printLog( ) {
+	int size = ( int )_log.size( );
+
+	for ( int i = 0; i < size; i++ ) {
+		int y = ( i % ( HEIGHT / 20 ) ) * 20;
+		DrawFormatString( 0, y, _color->getColor( WHITE ), "%s", _log[ i ].c_str( ) );
+	}
+}
+
+void Debug::initLog( ) {
+	std::vector< std::string >( ).swap( _log );
+}
+
+void Debug::addLog( std::string add ) {
+	_log.push_back( add );
 }
--- Solution/Mirror/Debug.h
+++ Solution/Mirror/Debug.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "Base.h"
+#include "Color.h"
+#include <memory>
+#include <vector>
+#include <string>
 #include <stdio.h>
 
 class Debug : public Base {
@@ -12,9 +16,17 @@ public:
 	void initialize( );
 
 public:
-	void error( char* err );
+	void error( std::string err );
+	void addLog( std::string add );
+
+private:
+	void printLog( );
+	void initLog( );
 
 private:
 	FILE *_fp;
+	std::vector< std::string > _log;
+
+	std::shared_ptr< Color > _color;
 };
 
