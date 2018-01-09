f5c635c8112e104b07087bb3830485e61f122c37
tika <a>
2018-01-09 17:37:24 +0900

    反射ベクトルが正しく返るに修正

--- Mirror/Common/Field.h
+++ Mirror/Common/Field.h
@@ -13,19 +13,24 @@ public:
 		double y;
 		Vector getReflection( Vector vec ) {
 			// R = F - 2dot( F, N )N
-			Vector F = { -1 * x, -1 * y };		//進行ベクトル
-			Vector N = vec;					//法線ベクトル
-			double dot = F.x * N.x + F.y * N.y;
 
-			Vector P =  Vector( );
-			P.x = F.x + dot * N.x;
-			P.y = F.y + dot * N.y;
-			P.x = -1;
-			P.y = 1;
+			//進行ベクトル
+			Vector F = { x, y };
 
-			Vector ref = Vector( );
-			ref.x = P.x + dot * N.x;
-			ref.y = P.y + dot * N.y;
+			//法線ベクトル
+			double length = sqrt( vec.x * vec.x + vec.y * vec.y );
+			Vector N = { vec.x / length, vec.y / length };
+
+			double dot = N.x * F.x + N.y * F.y;
+
+			Vector P = Vector( );
+			P.x = dot * N.x;
+			P.y = dot * N.y;
+
+			Vector ref = Vector(  );
+			ref.x = F.x - P.x * 2;
+			ref.y = F.y - P.y * 2;
+
 			return ref;
 		};
 	};
